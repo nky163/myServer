19b285bd9ac5652320e6b6c33cd1ed1a96e0def4
nky163 <nky8888163@gmail.com>
2021-05-29 19:39:29 +0900

    samba with docker

---
+++ samba/start_samba.sh
@@ -0,0 +1,10 @@
+echo start samba
+
+# set user and password
+adduser -D ${USER}
+echo -e ${PASSWORD}'\n'${PASSWORD} | pdbedit -a -u ${USER}
+
+nmbd restart --daemon
+smbd restart --daemon
+
+tail -f /dev/null
